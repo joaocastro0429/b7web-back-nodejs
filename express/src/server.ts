11aa44ac6f95d2255dbb6745de4e770dd3111454
joaocastro0429 <joaocastro0429@gmail.com>
2024-05-30 10:24:47 -0300

    add routes

--- express/src/server.ts
+++ express/src/server.ts
@@ -1,15 +1,33 @@
 
 import express from 'express'
 import helmet from 'helmet'
+import path from 'path'
 const server=express()
 server.use(helmet())
 server.use(express.json())
 server.use(express.urlencoded({extended:true}))
+server.use(express.static(path.join(__dirname,'../public')))
 
 
 server.get("/",(req,res)=>{
     res.send("hello word")
     
+})
+
+
+server.get("/produtos/:id",(req,res)=>{
+    console.log(req.params)
+    res.json({name:"mouse",price:90})
+    
+})
+
+server.get("/produtos",(req,res)=>{
+    res.send({produtos:[]})
+    
+})
+
+server.get("/ping",(req,res)=>{
+    res.json({pong:true})
 })
 
 server.listen(3333)
